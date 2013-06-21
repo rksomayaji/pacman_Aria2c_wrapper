da75420ebfca2aae5d03db8201ded0952c48ac42
rksomayaji <rksomayaji@gmail.com>
2013-06-21 20:06:49 +0530

    Handled issue where there is no upgrade

--- pacman_update.sh
+++ pacman_update.sh
@@ -1,9 +1,9 @@
 COL_LENGTH=$(tput cols)
 ONE_THIRD=$(($COL_LENGTH/3))
 HALF=$(($COL_LENGTH/2))
-TEMP_FILE=update.$(date +"%d%m%Y%H%M%S")
+UPDATE_FILE=update.$(date +"%d%m%Y%H%M%S")
 NO_FILES=1
-echo $TEMP_FILE
+echo $NO_FILES > /tmp/total_upgrades
 
 pacman -Sy
 
@@ -32,9 +32,9 @@ do
 		done
 	else
 		printf "%s%*s%*s\n" "$NAME" $(($HALF-${#NAME})) $VERSION $HALF $REPOS
-		echo $NO_FILES > /tmp/total_upgrades
-		NO_FILES=$(($NO_FILES+1))
-	fi
+        echo $NO_FILES > /tmp/total_upgrades
+        NO_FILES=$(($NO_FILES+1))
+    fi
 done
 
 for i in $(eval echo {1..$COL_LENGTH})
@@ -42,24 +42,34 @@ do
     printf "%s" "="
 done
 
-printf "\n"
-printf "\n%s%s\n\n" $(cat /tmp/total_upgrades) " files to be upgraded."
-rm /tmp/total_upgrades
+NO_UPDATES=$(cat /tmp/total_upgrades)
 
-select result in Yes No
-do
-case $result in
-       	Yes ) 
-		echo "Upgrading..."
-		mkdir /var/cache/pacman/$TEMP_FILE.d
-		pacman -Sup > /var/cache/pacman/$TEMP_FILE.d/$TEMP_FILE
-		sed -i 1d /var/cache/pacman/$TEMP_FILE.d/$TEMP_FILE
-		aria2c --dir=/var/cache/pacman/$TEMP_FILE.d/pkgs -i/var/cache/pacman/$TEMP_FILE.d/$TEMP_FILE -j10
-		cd /var/cache/pacman/$TEMP_FILE.d/pkgs
-		pacman --noconfirm -U *
-		break;;
-       	No ) 
-		echo "Cancelling upgrade..."
-		exit;;
-esac
-done
+if [ "$NO_UPDATES" = "1" ]
+then
+    printf "\n\n"
+    echo "No upgrades present at the moment..."
+    rm /tmp/total_upgrades
+    exit 1
+else
+    printf "\n"
+    printf "\n%s%s\n\n" $NO_UPDATES " files to be upgraded."
+    rm /tmp/total_upgrades
+
+    select result in Yes No
+    do
+    case $result in
+            Yes ) 
+            echo "Upgrading..."
+            mkdir /var/cache/pacman/$UPDATE_FILE.d
+            pacman -Sup > /var/cache/pacman/$UPDATE_FILE.d/$UPDATE_FILE
+            sed -i 1d /var/cache/pacman/$UPDATE_FILE.d/$UPDATE_FILE
+            aria2c --dir=/var/cache/pacman/$UPDATE_FILE.d/pkgs -i/var/cache/pacman/$UPDATE_FILE.d/$UPDATE_FILE -j10
+            cd /var/cache/pacman/$UPDATE_FILE.d/pkgs
+            pacman --noconfirm -U *
+            break;;
+            No ) 
+            echo "Cancelling upgrade..."
+            exit;;
+    esac
+    done
+fi
